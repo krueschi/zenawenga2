33db52856f26d733ede29cdf8fd96b597d61c4f0
Christian Boettger <krueschi@t-online.de>
2013-11-29 07:40:01 +0100

    Addidions for first public access test

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -30,24 +30,6 @@
       </div>
     <?php endif; ?>
 
-    <!-- secondary menu as a block
-    <?php if ($secondary_menu): ?>
-      <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
-        <?php print theme('links__system_secondary_menu', array(
-          'links' => $secondary_menu,
-          'attributes' => array(
-            'class' => array('links', 'inline', 'clearfix'),
-          ),
-          'heading' => array(
-            'text' => $secondary_menu_heading,
-            'level' => 'h2',
-            'class' => array('element-invisible'),
-          ),
-        )); ?>
-      </nav>
-    <?php endif; ?> -->
-    
-
     <?php print render($page['header']); ?>
 
   </header>
