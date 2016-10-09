eaf444b452028789bc1c4c5b6b6ebc4b38a3931a
krueschi <krueschi@t-online.de>
2016-10-09 23:06:00 +0200

    Added templates for simple page content (feature).

---
+++ zenawenga2/templates/paragraphs-item--seitentext_bild_links.tpl.php
@@ -0,0 +1,8 @@
+<div id="simplepageleftimage">
+  <?php print render($content['field_simplepage_subheader']); ?>
+  <div class="imageleft">
+    <?php print render($content['field_simplepage_picture']); ?>
+    <?php print render($content['field_simplepage_caption']); ?>
+  </div>
+  <?php print render($content['field_simplepage_textonly']); ?>
+</div>
---
+++ zenawenga2/templates/paragraphs-item--seitentext_bild_rechts.tpl.php
@@ -0,0 +1,8 @@
+<div id="simplepagerightimage">
+  <?php print render($content['field_simplepage_subheader']); ?>
+  <div class="imageright">
+    <?php print render($content['field_simplepage_picture']); ?>
+    <?php print render($content['field_simplepage_caption']); ?>
+  </div>
+  <?php print render($content['field_simplepage_textonly']); ?>
+</div>
