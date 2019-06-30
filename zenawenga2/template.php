2b4a309563c00f8a4299140cdd60a111172149ff
krueschi <krueschi@t-online.de>
2019-06-30 17:23:30 +0200

    Work in progress - new fonts pp.

--- zenawenga2/template.php
+++ zenawenga2/template.php
@@ -6,3 +6,15 @@
  * Complete documentation for this file is available online.
  * @see https://drupal.org/node/1728096
  */
+
+/**
+ * hook_preprocess_node(&$vars)
+ * Adding a custom template suggestion for teaser view modes.
+ * @see https://drupal.stackexchange.com/questions/27974/what-is-the-template-suggestion-for-node-teaser-view-mode
+ */
+function zenawenga2_preprocess_node(&$vars) {
+ if($vars['view_mode'] == 'teaser') {
+   array_unshift($vars['theme_hook_suggestions'], 'node__' . $vars['node']->type . '__teaser');
+   array_unshift($vars['theme_hook_suggestions'], 'node__' . $vars['node']->nid . '__teaser');
+ }
+}
